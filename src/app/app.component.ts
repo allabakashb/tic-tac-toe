85201d8892eb7040f2566d18506c652e9f26149d
Allabakash B <allabakash0@gmail.com>
2019-02-08 23:33:34 +0530

    Added changes for tic-tac-toe ui display changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, enableProdMode} from '@angular/core';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('fade-in', [
+      transition(':enter', [
+        style({ opacity: '0'}),
+        animate(2000)]),
+    ]),
+    trigger('stretch', [
+      transition(':enter', [
+        style({ transform: 'scale(1)'}),
+        animate(700)]),
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'tic-tac-toe';
+
+  blocks: any = Array.from(Array(3), () => Array(3).fill(''));
+  current: any = 'x';
+
+  constructor() {
+    console.log(this.blocks);
+  }
+
+  select(row, col): void {
+    if (!this.blocks[row][col]) {
+      this.blocks[row][col] = this.current;
+      this.current = this.current === 'x' ? 'o' : 'x';
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {enableProdMode, NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+enableProdMode();
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
